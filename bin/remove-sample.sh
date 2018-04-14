5a8da5f669f258f142b3960fe1fdeabe4d302920
Brian Grant <brian.grant@urbanairship.com>
2018-04-14 13:40:58 -0700

    fix string interporlation in if statements

--- bin/remove-sample.sh
+++ bin/remove-sample.sh
@@ -20,13 +20,13 @@ if [ -f ./Backups/dead_songs.sql ]; then
     rm ./Backups/dead_songs.sql
 fi
 
-if [ "docker ps -a -q -f ancestor=db_development --format=\"{{.ID}}\"" ]; then
+if [ "$(docker ps -a -q -f ancestor=db_development --format=\"{{.ID}}\")" ]; then
   echo "removing db container"
   docker stop $(docker ps -a -q  --filter ancestor=db_development --format="{{.ID}}")
   docker rm $(docker ps -a -q  --filter ancestor=db_development --format="{{.ID}}")
 fi
 
-if [ "docker ps -a -q -f name=api_development --format=\"{{.ID}}\"" ]; then
+if [ "$(docker ps -a -q -f name=api_development --format=\"{{.ID}}\")" ]; then
   echo "removing api container"
   docker stop $(docker ps -a -q  --filter name=api_development --format="{{.ID}}")
   docker rm $(docker ps -a -q  --filter name=api_development --format="{{.ID}}")
