4ba25107cc49c5f427fd806fc4b5d8dcaf959a77
Brian Grant <brian.grant@urbanairship.com>
2018-04-13 22:27:32 -0700

    update to crete script, stop container

--- bin/create-api-project.sh
+++ bin/create-api-project.sh
@@ -9,7 +9,8 @@ set -euo pipefail
 bin/remove-sample.sh
 
 # Create django project and api app
-docker-compose -f development-docker-compose.yml run api_development /bin/bash -c "django-admin.py startproject $PROJECT_NAME . ; ./manage.py startapp api"
+docker-compose -f development-docker-compose.yml run api_development /bin/bash -c "django-admin.py startproject $PROJECT_NAME . ; python manage.py startapp api"
+docker stop $(docker ps -a -q  --filter ancestor=db_development --format="{{.ID}}")
 
 # Replace newly created settings with the example settings
 rm $PROJECT_NAME/settings.py
