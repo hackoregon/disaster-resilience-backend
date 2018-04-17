08004523449b42a7802ee0837045f764968da91f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-04-16 23:45:58 -0700

    no more database container!!

--- bin/create-api-project.sh
+++ bin/create-api-project.sh
@@ -15,15 +15,9 @@ docker rmi db_development:latest api:latest || true
 
 echo "Creating new Django Rest Framework Project Scaffold..."
 echo "This will take some time"
-docker-compose -f development-docker-compose.yml run api_development \
+docker-compose -f development-docker-compose.yml run --no-deps --rm \
+  api_development \
   /bin/bash -c "django-admin.py startproject $PROJECT_NAME . ; python manage.py startapp api"
-
-echo "Saving database restore log"
-container_id=`docker ps | grep db_development | sed 's; .*$;;'`
-docker logs $container_id > db_development.log
-
-echo "Removing temporary containers..."
-docker-compose -f development-docker-compose.yml down
 
 # fix ownership
 echo "Fixing ownership on Linux"
