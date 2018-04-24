0a5a17eab5a41f70c7e5b36394c85b41b399b4b9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-04-24 13:17:40 -0700

    fix issue #40

--- bin/start.sh
+++ bin/start.sh
@@ -16,3 +16,13 @@ while getopts ":dp" opt; do
           ;;
     esac
 done
+
+# fix ownership
+echo "Fixing ownership on Linux"
+if [ `uname -s` = "Linux" ]
+then
+  ls -l
+  echo "sudo chown -R `id -u $USER`:`id -g $USER` ."
+  sudo chown -R `id -u $USER`:`id -g $USER` .
+  ls -l
+fi
