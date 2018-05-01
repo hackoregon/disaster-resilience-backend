d394080f8b04a624885a76fb5effd1fbeca8ebd2
nam20485 <nmiller217@gmail.com>
2018-05-01 15:07:12 -0700

    remove --rm flag to docker-compose move test_without_migrations to top of INSTALLED_APPS add URLs for Django unit test instructions/tutorial

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
 from api.models import preexisting_models
 
-class NeighborhoodUnitsTestCase(TestCase):
+# Django, Writing and Running Unit Tests: https://docs.djangoproject.com/en/2.0/topics/testing/overview/
+# Django, Automated Unit Testing Tutorial: https://docs.djangoproject.com/en/2.0/intro/tutorial05/
+
+class ExampleTestCase(TestCase):
     def setUp(self):
-        #preexisting_models.NeighborhoodUnits.objects.create()
         pass
 
     def test_example(self):
+        self.assertTrue(True)
+class ExampleModelTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_model(self):
         self.assertTrue(True)
