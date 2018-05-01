ae052ffce71d4c15063e7117617eb6c86f96a96f
nam20485 <nmiller217@gmail.com>
2018-05-01 16:56:07 -0700

    add example unit TestCases to demonstrate SimpleTestCase and TransactionTestCase

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, TransactionTestCase
 from api.models import preexisting_models
+
 
 # Django, Writing and Running Unit Tests: https://docs.djangoproject.com/en/2.0/topics/testing/overview/
 # Django, Automated Unit Testing Tutorial: https://docs.djangoproject.com/en/2.0/intro/tutorial05/
@@ -10,9 +11,21 @@ class ExampleTestCase(TestCase):
 
     def test_example(self):
         self.assertTrue(True)
+
 class ExampleModelTestCase(TestCase):
     def setUp(self):
         pass
 
     def test_model(self):
+        self.assertTrue(True)
+
+
+# https://docs.djangoproject.com/en/2.0/topics/testing/tools/#django.test.SimpleTestCase
+class ExampleSimpleTestCase(SimpleTestCase):
+    def test_simple_example(self):
+        self.assertTrue(True)
+
+# https://docs.djangoproject.com/en/2.0/topics/testing/tools/#transactiontestcase
+class ExampleTransactionTestCase(TransactionTestCase):
+    def test_transaction_example(self):
         self.assertTrue(True)
