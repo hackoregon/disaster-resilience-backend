25223e8bc0e05e051c08a34f08e0b8a52ddd2138
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-05 23:37:25 -0700

    Removing leftover code we're no longer using

--- bin/test.sh
+++ bin/test.sh
@@ -7,17 +7,9 @@ while getopts ":dp" opt; do
     case "$opt" in
         d)
           docker-compose -f development-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh api_development -p 8000
-          #echo "Stopping test db container"
-          #docker stop api_development
-          #echo "Removing test db container"
-          #docker rm api_development
           ;;
         p)
           docker-compose -f production-docker-compose.yml run --entrypoint /code/bin/test-entrypoint.sh api_production -p 8000
-          #echo "Stopping test db container"
-          #docker stop api_production
-          #echo "Removing test db container"
-          #docker rm api_production
           ;;
         *)
           usage
