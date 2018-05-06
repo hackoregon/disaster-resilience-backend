d1c2702e296c1835d24d7b4027c7ecae623ef473
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-05 23:33:06 -0700

    Remove wait-for-it from production docker-entrypoint script too - just as pointless in ECS environment

--- bin/production-docker-entrypoint.sh
+++ bin/production-docker-entrypoint.sh
@@ -1,20 +1,8 @@
 #! /bin/bash
 
-# wait-for-postgres.sh
-# https://docs.docker.com/compose/startup-order/
-
 # http://linuxcommand.org/lc3_man_pages/seth.html:
 # -e  Exit immediately if a command exits with a non-zero status.
 set -e
-
-export PGPASSWORD=$POSTGRES_PASSWORD
-until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d "$POSTGRES_NAME" -c '\q'
-do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 5
-done
-
->&2 echo "Postgres is up"
 
 echo Debug: $DEBUG
 
