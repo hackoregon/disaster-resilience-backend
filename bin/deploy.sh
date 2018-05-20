c61be595c22887929cc3eeb34d35f064cd736d09
Nathan Miller <nmiller217@gmail.com>
2018-05-20 05:21:30 -0700

    Remove PRODUCTION_ prefix from docker image and service name variables change $PRODUCTION_DOCKER_IMAGE and $PRODUCTION_DOCKER_SERVICE to $DOCKER_IMAGE and $DOCKER_SERVICE

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,18 +12,18 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo Getting the ECR login...
         eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
-        REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$PRODUCTION_DOCKER_IMAGE"
+        REMOTE_DOCKER_PATH="$DOCKER_REPO"/"$DOCKER_REPO_NAMESPACE"/"$DOCKER_IMAGE"
         
         # tag with branch and travis build number then push
         TAG=travis-buildnum-"$TRAVIS_BUILD_NUMBER"
         echo Tagging with "$TAG"
-        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"    
+        docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"    
         docker push "$REMOTE_DOCKER_PATH":"$TAG"
 
         # tag with "latest" then push
         TAG=latest
         echo Tagging with "$TAG"
-        docker tag "$PRODUCTION_DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
+        docker tag "$DOCKER_IMAGE":latest "$REMOTE_DOCKER_PATH":"$TAG"
         docker push "$REMOTE_DOCKER_PATH":"$TAG"
         
         #echo Running ecs-deploy.sh script...
