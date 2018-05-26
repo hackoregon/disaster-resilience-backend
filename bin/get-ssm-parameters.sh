8442018641077bfa44fb4bb0468fae6ea81d9dcd
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-26 13:05:01 -0700

    stupid typos broke the up-front env var declarations

--- bin/get-ssm-parameters.sh
+++ bin/get-ssm-parameters.sh
@@ -10,9 +10,9 @@
 # 2. echo out the parameter values so they can be used by the container apps
 # 3. determine the best place to run this script so that it populates the env vars before they're needed by other code
 
-EC2_REGION = "us-west-2" # unfortunately cannot rely on dynamic env var values that this script is meant to pull in
-NAMESPACE = "/production/2018/API" # future-proofing this script for subsequent or past containers
-PROJECT_NAME = "disaster-resilience" # must be set to each project's "Final naming convention" from here https://github.com/hackoregon/civic-devops/issues/1
+EC2_REGION="us-west-2" # unfortunately cannot rely on dynamic env var values that this script is meant to pull in
+NAMESPACE="/production/2018/API" # future-proofing this script for subsequent or past containers
+PROJECT_NAME="disaster-resilience" # must be set to each project's "Final naming convention" from here https://github.com/hackoregon/civic-devops/issues/1
 
 # Get unencrypted values
 POSTGRES_HOST=`aws ssm get-parameters --names "$NAMESPACE/$PROJECT_NAME/POSTGRES_HOST" --no-with-decryption --region $EC2_REGION --output text 2>&1`
