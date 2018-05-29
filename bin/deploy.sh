09c962befc7c0cace15d1c321e67936dbc8cc541
Nathan Miller <nmiller217@gmail.com>
2018-05-29 14:52:56 -0700

    remove printing DJANGO_SECRET_KEY to stdout

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,10 +4,10 @@
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     # Push only if we're testing the master branch
-    #if [ "$TRAVIS_BRANCH" == "master" ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
 
         # remove until such time as anybody can remember why it is here
-        # export PATH=$PATH:$HOME/.local/bin
+        # export PATH=$PATH:$HOME/.local/binw
 
         echo Getting the ECR login...
         eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
@@ -32,9 +32,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
            --cluster "$ECS_CLUSTER"   \
            --image "$REMOTE_DOCKER_PATH":latest \
            --timeout 300
-    #else
-    #    echo "Skipping deploy because branch is not master"
-    #fi
+    else
+        echo "Skipping deploy because branch is not master"
+    fi
 else
     echo "Skipping deploy because it's a pull request"
 fi
--- bin/production-docker-entrypoint.sh
+++ bin/production-docker-entrypoint.sh
@@ -1,20 +1,15 @@
 #! /bin/bash
 
-
 # http://linuxcommand.org/lc3_man_pages/seth.html:
 # -e  Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo Debug: $DEBUG
-
 # Pull in environment variables values from AWS Parameter Store, and preserve the exports
-# source usage per https://stackoverflow.com/q/14742358/452120
-source /code/bin/get-ssm-parameters.sh
-
-# Temporary troubleshooting
-echo "from entrypoint script DJANGO_SECRET_KEY value is " $DJANGO_SECRET_KEY
+# source usage per https://stackoverflow.com/q/14742358/452120 (iff running on travis-ci) 
+if [ -n "$TRAVIS" ] && [ "$TRAVIS" == "true" ]; then
+    source /code/bin/get-ssm-parameters.sh
+fi
 
 python -Wall manage.py collectstatic --noinput
-
 
 gunicorn $PROJECT_NAME.wsgi -c gunicorn_config.py
