e3bd8f3dace2a23a3b477ae98748103a486eca43
Nathan Miller <nmiller217@gmail.com>
2018-07-29 20:00:47 -0700

    add trailing backslashes to avoid HTTP_301 use status response constants

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, SimpleTestCase, TransactionTestCase
 from api.models import preexisting_models
-from rest_framework.test import APIClient, RequestsClient
+from rest_framework.test import APIClient
+from rest_framework import status
 
 # Django, Writing and Running Unit Tests: https://docs.djangoproject.com/en/2.0/topics/testing/overview/
 # Django, Automated Unit Testing Tutorial: https://docs.djangoproject.com/en/2.0/intro/tutorial05/
@@ -10,21 +11,21 @@ class RootEndpointTestCase(TestCase):
         self.client = APIClient()
     def test_list_response(self):
         response = self.client.get('/disaster-resilience/')
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
 
 class APIRootEndpointTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
     def test_response(self):
-        response = self.client.get('/disaster-resilience/api')
-        assert response.status_code == 200
+        response = self.client.get('/disaster-resilience/api/')
+        assert response.status_code == status.HTTP_200_OK
 
 class DocsEndpointTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
     def test_response(self):
-        response = self.client.get('/disaster-resilience/docs')
-        assert response.status_code == 200
+        response = self.client.get('/disaster-resilience/docs/')
+        assert response.status_code == status.HTTP_200_OK
 
 class SandboxAPIEndpointsTestCase(TestCase):
     def setUp(self):
@@ -32,12 +33,12 @@ class SandboxAPIEndpointsTestCase(TestCase):
         self.datasets = ['foundations/censusresponse','foundations/landslide/','foundations/liquefaction','foundations/shaking','slides/poi/']
     def test_list_responses(self):
         for endpoint in self.datasets:
-            response = self.client.get('/disaster-resilience/sandbox/'+endpoint)
-            assert response.status_code == 200
+            response = self.client.get('/disaster-resilience/sandbox/'+endpoint+'/')
+            assert response.status_code == status.HTTP_200_OK
     def test_detail_responses(self):
         for endpoint in self.datasets:
-            response = self.client.get('/disaster-resilience/sandbox/'+endpoint+"/1")
-            assert response.status_code == 200
+            response = self.client.get('/disaster-resilience/sandbox/'+endpoint+"/1/")
+            assert response.status_code == status.HTTP_200_OK
 
 class APIEndpointsTestCase(TestCase):
     def setUp(self):
@@ -45,10 +46,10 @@ class APIEndpointsTestCase(TestCase):
         self.datasets = ['NeighborhoodUnits', 'BuildingFootprints', 'ElectricalTransmissionStructures', 'Jurisdictions', 'QuakeLossView', 'PhfM6P8BedrockGroundmotion', 'PopulationAndBuildingDensity', 'CensusBgAoi', 'CommunityCenters', 'Hospital', 'OregonLiquefactionSusceptibility', 'OregonNehrpSiteClass', 'OregonVsMeasurementIntervals', 'OregonVsMeasurementSites', 'UnreinforcedMasonryBuildings', 'Address', 'FireSta', 'Schools', 'NeighborhoodsRegions', 'MajorRiverBridges', 'BasicEarthquakeEmergencyCommunicationNodeBeecnLocations', 'RlisSt180520', 'POI', 'DisasterNeighborhoods', 'DisasterNeighborhoodView', 'DisasterNeighborhoodGrid']
     def test_list_responses(self):
         for endpoint in self.datasets:
-            response = self.client.get('/disaster-resilience/api/'+endpoint)
-            assert response.status_code == 200
+            response = self.client.get('/disaster-resilience/api/'+endpoint+'/')
+            assert response.status_code == status.HTTP_200_OK
     def test_detail_responses(self):
         for endpoint in self.datasets:
-            response = self.client.get('/disaster-resilience/api/'+endpoint+"/1")
-            assert response.status_code == 200
+            response = self.client.get('/disaster-resilience/api/'+endpoint+"/1/")
+            assert response.status_code == status.HTTP_200_OK
         
