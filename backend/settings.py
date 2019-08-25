515aa295407aadfaba04512db2b46f2d24bd84f3
Brian Grant <brian.grant@urbanairship.com>
2019-08-25 13:43:47 -0700

    set debug = true

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == "True"
+# DEBUG = os.environ.get('DEBUG') == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
